8a0ad90a950b7b7aa5e4ef60fdb311db6bccd6a0
Vedant Chavda <f20180209@goa.bits-pilani.ac.in>
2020-01-19 12:09:57 +0530

    addcheckandtempl

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define ll long long int
+int main()
+{
+	ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);    
+
+    
+
+	return 0;
+}
